d7f2b221a1d06824c552d02128b3e23dce08e6c9
Ryan Ardiansyah <125651352+RyanArdiansyah10@users.noreply.github.com>
2023-05-24 21:04:57 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,22 +1,23 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, davies_bouldin_score
 
-dataset = pd.read_csv('InternetUser.csv')
+dataset = pd.read_csv('gender.csv')
 
 print('Sample Data')
 print(dataset.head())
 
-print(dataset.isna().sum())
+dataset.isna().sum()
 
-print(dataset.dtypes)
+dataset.dtypes
 
 x = dataset.iloc[:, :4]
-y = dataset['Other_Social_Network']
+y = dataset['gender']
 
 xTrain, xTest, yTrain, yTest = train_test_split(
     x, y, test_size=0.3, random_state=0)
@@ -24,36 +25,28 @@ xTrain, xTest, yTrain, yTest = train_test_split(
 dt = DecisionTreeClassifier()
 dt.fit(xTrain, yTrain)
 
-print('Decision Tree Accuracy: {:.3f}'.format(
+print('Decisoin Tree Accuracy: {:.3f}'.format(
     accuracy_score(yTest, dt.predict(xTest))))
 
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(5, 5))
 tree.plot_tree(dt, filled=True)
 
-kMeans = KMeans(n_clusters=3)
-labels = kMeans.fit_predict(x)
+KMeans = KMeans(n_clusters=3)
+labels = KMeans.fit_predict(x)
 
 db_score = davies_bouldin_score(x, labels)
-print('Davies Bouldin Score:', db_score)
+print(db_score)
 
 cols = dataset.columns
 plt.scatter(x.loc[labels == 0, cols[0]],
             x.loc[labels == 0, cols[1]],
-            s=100, c='purple',
-            label='Cluster 0')
+            s=100, c='purple', label='setosa')
 plt.scatter(x.loc[labels == 1, cols[0]],
             x.loc[labels == 1, cols[1]],
-            s=100, c='orange',
-            label='Cluster 1')
+            s=100, c='orange', label='versicolour')
 plt.scatter(x.loc[labels == 2, cols[0]],
             x.loc[labels == 2, cols[1]],
-            s=100, c='green',
-            label='Cluster 2')
-
-plt.scatter(kMeans.cluster_centers_[:, 0],
-            kMeans.cluster_centers_[:, 1],
-            s=100, c='red',
-            label='Centroids')
-
-plt.legend()
-plt.show()
+            s=100, c='green', label='virginica')
+plt.scatter(KMeans.cluster_centers_[:, 0],
+            KMeans.cluster_centers_[:, 1],
+            s=100, c='red', label='centroids')
